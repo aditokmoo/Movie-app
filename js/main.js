8fb3f92bac8b8962a66edb6daaf272c0cf7737e0
Havock48 <adit.github@gmail.com>
2022-10-07 00:05:27 +0200

    Pagination addition added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-// API URL
 let currentPage = 1;
-const api_url = `https://api.themoviedb.org/3/movie/popular?api_key=a2949ba2bbc81404864f35921a20a1d0&language=en-US&page=${currentPage}`
+let api_url = `https://api.themoviedb.org/3/movie/popular?api_key=a2949ba2bbc81404864f35921a20a1d0&language=en-US&page=${currentPage}`
 const search_url = 'https://api.themoviedb.org/3/search/movie?api_key=a2949ba2bbc81404864f35921a20a1d0&query='
 const img_path = 'https://image.tmdb.org/t/p/w1280';
 const movies_section = document.querySelector('.movies-section');
@@ -12,6 +11,14 @@ window.addEventListener('load', () => {
     hideLoader();
 });
 
+// Get Movies Function
+async function getMovies(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    displayMovie(data);
+}
+
 // Pagination Buttons
 const fowardBtn = document.querySelector('#foward-btn');
 const backwardBtn = document.querySelector('#backward-btn');
@@ -19,27 +26,40 @@ const page = document.querySelector('#pagination_page');
 // Pagination Function
 async function pagination() {
     const api = `https://api.themoviedb.org/3/movie/popular?api_key=a2949ba2bbc81404864f35921a20a1d0&language=en-US&page=${currentPage}`;
-        const res = await fetch(api);
-        const data = await res.json();
-        page.innerText = currentPage;
+    const res = await fetch(api);
+    const data = await res.json();
+    page.innerText = currentPage;
 
-        displayLoader();
+    displayLoader();
 
-        displayMovie(data);
+    displayMovie(data);
 
-        setTimeout(() => {
-            hideLoader();
-        }, 500);
+    setTimeout(() => {
+        hideLoader();
+    }, 500);
 }
 
+// Disable Pagination Function
+function disablePagination() {
+    if(currentPage === 1) {
+        backwardBtn.classList.remove('disabled')
+    } else {
+        backwardBtn.classList.add('disabled')
+    }
+}
+
+// Pagination button for back
 backwardBtn.addEventListener('click', (e) => {
     e.preventDefault();
+    disablePagination();
     currentPage--;
     pagination();
 })
 
+// Pagination button for foward
 fowardBtn.addEventListener('click', (e) => {
     e.preventDefault();
+    disablePagination();
     currentPage++;
     pagination();
 });
@@ -62,14 +82,6 @@ function displayLoader() {
     
     movies_section.style.display = 'none'
     setTimeout(() => {movies_section.style.opacity = 0}, 50)
-}
-
-// Get Movies Function
-async function getMovies(url) {
-    const res = await fetch(url);
-    const data = await res.json();
-    
-    displayMovie(data);
 }
 
 // Search Movie
@@ -100,24 +112,24 @@ function displayMovie(movies) {
 
     movies.results.forEach(item => {
         const { title, poster_path, vote_average, id } = item;
-        
-        movies_section.innerHTML += `
-            <div class="movie">
-                <div class="card-inner">
-                    <div class="card front">
-                        <img src="${img_path + poster_path}" alt="">
-                        <div class="movie-info">
-                            <h3>${title}</h3>
-                            <span class="${getClassByRating(vote_average)}">${vote_average}</span>
+
+            movies_section.innerHTML += `
+                <div class="movie">
+                    <div class="card-inner">
+                        <div class="card front">
+                            <img src="${img_path + poster_path}" alt="">
+                            <div class="movie-info">
+                                <h3>${title}</h3>
+                                <span class="${getClassByRating(vote_average)}">${vote_average}</span>
+                            </div>
+                        </div>
+                        <div class="card back" style="background-image: url('${img_path + poster_path}')">
+                            <div class="overlay"></div>
+                            <a href="#" id="movie-modal-${id}">Check</a>
                         </div>
                     </div>
-                    <div class="card back" style="background-image: url('${img_path + poster_path}')">
-                        <div class="overlay"></div>
-                        <a href="#" id="movie-modal-${id}">Check</a>
-                    </div>
                 </div>
-            </div>
-        `
+            `
     });
 
     openMovie(movies);
